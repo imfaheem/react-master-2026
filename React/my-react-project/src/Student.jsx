31ac8f2d3cda08190a28d5f0fde4c6bcb25f966d
Muhammad Faheem Sikandar <faheem9t@gmail.com>
2026-07-14 20:57:17 +0500

    Day 7 Done. React JS started.

---
+++ React/my-react-project/src/Student.jsx
@@ -0,0 +1,9 @@
+export const Student = ({name, age, course}) => {
+    return (
+        <div>
+            <h4>Name: {name}</h4>
+            <h4>Age: {age}</h4>
+            <h4>Course: {course}</h4>
+        </div>
+    )
+}
